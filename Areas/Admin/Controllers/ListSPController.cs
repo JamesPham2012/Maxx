8ef06c4f847aaefa1e0da48d4fb4ab5a0aad9e29
VGUlCS2018Uniform <52058856+VGUlCS2018Uniform@users.noreply.github.com>
2022-03-02 22:14:15 +0700

    Initial commit

---
+++ Areas/Admin/Controllers/ListSPController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanHang.Models;
+using WebSiteBanHang.Models.ViewMD;
+
+namespace WebSiteBanHang.Areas.Admin.Controllers
+   
+{
+    public class ListSPController : Controller
+    {
+        private QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        // GET: Admin/ListSP
+        public ActionResult Index()
+        {
+            ListSanPhamAD_MD listSP = new ListSanPhamAD_MD();
+            IEnumerable<ListSanPhamAD_MD> ListSP = (from objSp in db.SanPhams
+                                                    join objLoai in db.LoaiSanPhams
+                                                    on objSp.MaLoaiSP equals objLoai.MaLoaiSP
+                                                    select new ListSanPhamAD_MD()
+                                                    {
+                                                        MaSP = objSp.MaSP,
+                                                        TenSP = objSp.TenSP,
+                                                        DonGia = objSp.DonGia,
+                                                        MoTa = objSp.MoTa,
+                                                        HinhAnh = objSp.HinhAnh,
+                                                        TenLoai = objLoai.TenLoai,
+                                                    }).ToList();
+
+            return View(ListSP);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            Debug.WriteLine(db);
+            SanPham sanPhamCanXoa = db.SanPhams.SingleOrDefault(n => n.MaSP==id);
+            Debug.WriteLine("sp "+ sanPhamCanXoa.MaSP);
+            db.SanPhams.Remove(sanPhamCanXoa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/ViewMD/ListSanPhamAD_MD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSiteBanHang.Models.ViewMD
+{
+    public class ListSanPhamAD_MD
+    {
+        public int MaSP { get; set; }
+        public string TenSP { get; set; }
+        public Nullable<decimal> DonGia { get; set; }
+        public string MoTa { get; set; }
+        public string HinhAnh { get; set; }
+        public string TenLoai { get; set; }
+    }
+}
