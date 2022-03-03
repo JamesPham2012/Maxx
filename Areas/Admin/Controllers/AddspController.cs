2d44eeb0673f05eeb3a9333209d2e6f84951e413
Roxxel012 <baocon011@gmail.com>
2022-03-04 02:11:00 +0700

    sas

---
+++ Areas/Admin/Controllers/AddspController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanHang.Models.ViewMD;
+using WebSiteBanHang.Models;
+using System.IO;
+
+namespace WebSiteBanHang.Areas.Admin.Controllers
+{
+    public class AddspController : Controller
+    {
+        private QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        // GET: Admin/Addsp
+        public ActionResult Index()
+        {
+            ThemSPMD dbmd = new ThemSPMD();
+            dbmd.AddLoai = (from objaddloai in db.LoaiSanPhams
+                            select new SelectListItem()
+                            {
+                                Text = objaddloai.TenLoai,
+                                Value = objaddloai.MaLoaiSP.ToString(),
+                                Selected = true,
+                            });
+
+
+            return View(dbmd);
+        }
+        [HttpPost]
+        public JsonResult Index(ThemSPMD themmd)
+        {
+           
+            SanPham sp = new SanPham();
+         
+            sp.MaSP = themmd.MaSP;
+            sp.TenSP = themmd.TenSP;
+            sp.HinhAnh = themmd.HinhAnh;
+            sp.DonGia = themmd.DonGia;
+            sp.NgayCapNhat = themmd.NgayCapNhat;
+            sp.CauHinh = themmd.CauHinh;
+            sp.MoTa = themmd.MoTa;
+            sp.SoLuongTon = themmd.SoLuongTon;
+            sp.LuotXem = themmd.LuotXem;
+            sp.LuotBinhChon = themmd.LuotBinhChon;
+            sp.LuotBinhLuan = themmd.LuotBinhLuan;
+            sp.SoLanMua = themmd.SoLanMua;
+            sp.Moi = themmd.Moi;
+            sp.MaNCC = themmd.MaNCC;
+            sp.MaNSX = themmd.MaNSX;
+            sp.MaLoaiSP = themmd.MaLoaiSP;
+            sp.DaXoa = themmd.DaXoa;
+           
+         
+            db.SanPhams.Add(sp);
+            db.SaveChanges();
+            return Json(new { Success = true, Message = "Add Success" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Models/ViewMD/ThemSPMD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSiteBanHang.Models.ViewMD
+{
+    public class ThemSPMD
+    {
+        public int MaSP { get; set; }
+        public string TenSP { get; set; }
+        public Nullable<decimal> DonGia { get; set; }
+        public Nullable<System.DateTime> NgayCapNhat { get; set; }
+        public string CauHinh { get; set; }
+        public string MoTa { get; set; }
+        public string HinhAnh { get; set; }
+        public Nullable<int> SoLuongTon { get; set; }
+        public Nullable<int> LuotXem { get; set; }
+        public Nullable<int> LuotBinhChon { get; set; }
+        public Nullable<int> LuotBinhLuan { get; set; }
+        public Nullable<int> SoLanMua { get; set; }
+        public Nullable<int> Moi { get; set; }
+        public Nullable<int> MaNCC { get; set; }
+        public Nullable<int> MaNSX { get; set; }
+        public Nullable<int> MaLoaiSP { get; set; }
+        public Nullable<bool> DaXoa { get; set; }
+
+
+        public IEnumerable<SelectListItem> AddLoai { get; set; }
+
+
+    }
+}
